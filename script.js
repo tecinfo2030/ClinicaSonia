d15366c327b8b8b9b59fc66df4d7a1a6c5cd72b5
tecinfo2030 <tecinfo2025@gmail.com>
2025-03-02 11:23:42 -0300

    feat: Efeito Fumê -adicionada div overlay - modificada a função alternarOpcoes() para fazer o overlay aparecer - alterado z-index das classes para que o orverlay cubra todo body

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ console.log('Script carregado com sucesso');
 function alternarOpcoes() {
     let botaoCamera = document.getElementById('botaoCamera');
     let botaoPin = document.getElementById('botaoPin');
+    let overlay = document.getElementById("overlay");
 
    if (botaoCamera.style.display === 'none' || botaoCamera.style.display === '') {
         botaoCamera.style.display = 'inline-block';
         botaoPin.style.display = 'inline-block';
+        overlay.style.display = "block";
    } else {
         botaoCamera.style.display = 'none';
         botaoPin.style.display = 'none';
+        overlay.style.display = 'none';
    }
 }
 
