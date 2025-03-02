398d7d595916f40a31c4a44eb8a05ed2c0b0cb43
tecinfo2030 <tecinfo2025@gmail.com>
2025-03-02 11:12:19 -0300

    feat: criado botões para consulta de video e consulta presencial -criado arquivo css para os botões -criada função JS alternarOpcoes() para os novos botões só aparecerem quando o botão de adionar consulta for clicado.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log('Script carregado com sucesso');
+
+function alternarOpcoes() {
+    let botaoCamera = document.getElementById('botaoCamera');
+    let botaoPin = document.getElementById('botaoPin');
+
+   if (botaoCamera.style.display === 'none' || botaoCamera.style.display === '') {
+        botaoCamera.style.display = 'inline-block';
+        botaoPin.style.display = 'inline-block';
+   } else {
+        botaoCamera.style.display = 'none';
+        botaoPin.style.display = 'none';
+   }
+}
+
+
